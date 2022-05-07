1bbf740d3cd52774d195f4e8781740de6578371d
Tolfx <matteus@tolfx.dev>
2022-05-07 15:46:29 +0200

    feat: :sparkles: Testing logo for customer (#3)

--- apps/customer/pages/api/info/company.ts
+++ apps/customer/pages/api/info/company.ts
@@ -7,9 +7,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) =>
     {
         const QUERY_STRING_PARAMETERS = <ICompanyData>{
             COMPANY_NAME: process.env.COMPANY_NAME || 'CPG',
-            COMPANY_LOGO: process.env.COMPANY_LOGO || '',
-            CPG_DOMAIN: process.env.CPG_DOMAIN || '',
+            COMPANY_LOGO: process.env.COMPANY_LOGO || 'https://cdn.tolfix.com/cpg/logo.png',
+            CPG_DOMAIN: process.env.CPG_DOMAIN || 'http://localhost:3001',
         };
+        process.env.CPG_DOMAIN = QUERY_STRING_PARAMETERS.CPG_DOMAIN;
 
         return res.json(QUERY_STRING_PARAMETERS);
     }
