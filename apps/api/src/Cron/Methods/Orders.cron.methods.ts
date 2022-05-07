f8bb5ab97e2cb9a340d58565135f43d0a83d9d79
Tolfx <matteus@tolfx.dev>
2022-05-07 19:59:27 +0200

    feat: :sparkles: Progress on issue #6

--- apps/api/src/Cron/Methods/Orders.cron.methods.ts
+++ apps/api/src/Cron/Methods/Orders.cron.methods.ts
@@ -6,6 +6,8 @@ import dateFormat from "date-and-time";
 import nextRecycleDate from "../../Lib/Dates/DateCycle";
 import { createInvoiceFromOrder } from "../../Lib/Orders/newInvoice";
 import { InvoiceCreatedReport } from "../../Email/Reports/InvoiceReport";
+import { ICustomer } from "interfaces/Customer.interface";
+import { IOrder } from "interfaces/Orders.interface";
 
 // Logger.info(`Checking orders..`);
 export function cron_createNewInvoicesFromOrders()
@@ -20,6 +22,7 @@ export function cron_createNewInvoicesFromOrders()
     }).then(async orders =>
     {
         const newInvoices = [];
+        const cOrder = new Map<ICustomer["uid"], IOrder[]>();
         // orders.forEach(async order => {
         for await (const order of orders)
         {
@@ -35,21 +38,111 @@ export function cron_createNewInvoicesFromOrders()
                     order.dates.next_recycle = dateFormat.format(nextRecycleDate(
                         dateFormat.parse(temptNextRecycle, "YYYY-MM-DD"), order.billing_cycle ?? "monthly")
                         , "YYYY-MM-DD");
-                    // Create a new invoice
+
+                    if (!cOrder.get(order.customer_uid))
+                        cOrder.set(order.customer_uid, [order]);
+                    else
+                        cOrder.set(order.customer_uid, [
+                            // @ts-ignore
+                            ...cOrder.get(order.customer_uid),
+                            order
+                        ]);
+
+                    order.markModified("dates");
+                    // Save the order
+                    // await order.save();
+                }
+        }
+        // Get our orders from each customer,
+        // There we will check which has the exactly the same dates and is recurring as well
+        // And if they have the same dates and is recurring we join them in one invoice
+        // Otherwise we just create a new invoice 
+        cOrder.forEach(async (orders) =>
+        {
+            // Go through each orders and check if any has the same dates
+            // If they have the same dates we join them in one invoice
+            // Otherwise we just create a new invoice
+            const ordersToInvoice = [];
+            const rest = [];
+            const allOrdersToSave = [];
+            for await (const order of orders)
+            {
+                // Check if the order is recurring
+                if (order.billing_cycle)
+                {
+                    // Check if the order has the same dates
+                    if (orders.every(o =>
+                    {
+                        return (o.dates.next_recycle === order.dates.next_recycle) ||
+                            (o.dates.last_recycle === order.dates.last_recycle) ||
+                            // Check if same currency
+                            (o.currency === order.currency);
+                    }))
+                    {
+                        // Check if the order is recurring
+                        if (order.billing_cycle)
+                        {
+                            ordersToInvoice.push(order);
+                            continue;
+                        }
+                    }
+                }
+                rest.push(order);
+            }
+
+            // Now we loop through each to create a "fake",
+            // order with all of them combined in one invoice
+            if (ordersToInvoice.length > 0)
+            {
+
+                const nOrder = ordersToInvoice[0];
+                // @ts-ignore
+                // Check each product, and if they are the same add quantity
+                nOrder.products = ordersToInvoice.reduce((acc, curr) =>
+                {
+                    acc.forEach(p =>
+                    {
+                        curr.products.forEach(c =>
+                        {
+                            if (p.product_uid === c.product_uid)
+                            {
+                                p.quantity += c.quantity;
+                            }
+                        }
+                        );
+                    }
+                    );
+                    return acc;
+                }, nOrder.products);
+                // Create a new invoice
+                const newInvoice = await createInvoiceFromOrder(nOrder);
+                newInvoices.push(newInvoice);
+                // Add the order to the list of orders to save
+                for (const order of ordersToInvoice)
+                {
+                    order.invoices.push(newInvoice.id);
+                    allOrdersToSave.push(order);
+                }
+            }
+
+            if (rest.length > 0)
+            {
+                // Create a new invoice
+                for await (const order of rest)
+                {
                     const newInvoice = await createInvoiceFromOrder(order);
                     newInvoices.push(newInvoice);
-
-                    // Save the invoice in order.invoices array
                     order.invoices.push(newInvoice.id);
-
-                    // mark order updated in dates
-                    order.markModified("dates");
-                    order.markModified("invoices");
-                    // Save the order
-                    await order.save();
+                    allOrdersToSave.push(order);
                 }
-            if (newInvoices.length > 0)
-                await InvoiceCreatedReport(newInvoices);
-        }
+            }
+
+            // Save all the orders
+            for await (const order of allOrdersToSave)
+                await OrderModel.findOneAndUpdate({ id: order.id }, order)
+
+        });
+        if (newInvoices.length > 0)
+            await InvoiceCreatedReport(newInvoices);
     });
 }
