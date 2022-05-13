e0ab0e218bc04d5174a54ef32442ac987679eddd
Tolfx <matteus@tolfx.dev>
2022-05-13 21:31:28 +0200

    feat: :sparkles: Added `RenderFullName` method

---
+++ apps/admin/src/lib/RenderFullName.ts
@@ -0,0 +1,8 @@
+import { ICustomer } from "interfaces/Customer.interface";
+
+export default function <T extends ICustomer>(choice: T)
+{
+    if (!choice)
+        return ``;
+    return `${choice?.personal?.first_name ?? ``} ${choice?.personal?.last_name ?? ``}`;
+}
