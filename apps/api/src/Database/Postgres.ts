237d2c449e9279696b91f6aaed1e00aa4ad480bf
Tolfx <matteus@tolfx.dev>
2022-05-18 11:07:31 +0200

    fix: :fire: Removed postgres

--- apps/api/src/Database/Postgres.ts
+++
@@ -1,4 +0,0 @@
-import { Sequelize } from "sequelize";
-import { Postgres_Database, Postgres_Host, Postgres_Password, Postgres_Port, Postgres_User } from "../Config";
-
-export const postgres = new Sequelize(`postgres://${Postgres_User}:${Postgres_Password}@${Postgres_Host}:${Postgres_Port}/${Postgres_Database}`);
