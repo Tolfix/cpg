5fe983700f9a551ada039d5bdd89b69888de1c5f
Tolfx <matteus@tolfx.dev>
2022-05-18 21:30:10 +0200

    fix: :sparkles: Invoice.amount to invoice.items

--- apps/api/src/Payments/Stripe.ts
+++ apps/api/src/Payments/Stripe.ts
@@ -90,7 +90,12 @@ export const CreatePaymentIntent = async (invoice: IInvoice) =>
     customer.extra.stripe_id = s_customer?.id;
     customer.markModified("extra");
     await customer.save();
-    const fAmount = parseInt(invoice.amount.toFixed(2));
+
+    const fAmount = parseInt(invoice.items.reduce((acc, item) =>
+    {
+        return acc + item.amount * item.quantity;
+    }, 0).toFixed(2));
+
     const intent = (await Stripe.paymentIntents.create({
         customer: s_customer?.id,
         amount: (fAmount + fAmount * invoice.tax_rate / 100) * 100,
