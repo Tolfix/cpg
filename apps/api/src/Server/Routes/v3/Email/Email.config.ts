786072545fb02cd3ec5b9d811a5115873b1f3ecc
Tolfx <matteus@tolfx.dev>
2022-05-27 17:56:34 +0200

    feat: :art: Improved email api (#45)

--- apps/api/src/Server/Routes/v3/Email/Email.config.ts
+++ apps/api/src/Server/Routes/v3/Email/Email.config.ts
@@ -1,7 +1,8 @@
 import { Application, Router } from "express";
 import CustomerModel from "../../../../Database/Models/Customers/Customer.model";
 import { sendEmail } from "../../../../Email/Send";
-import { APISuccess } from "../../../../Lib/Response";
+import UseStyles from "../../../../Email/Templates/General/UseStyles";
+import { APIError, APISuccess } from "../../../../Lib/Response";
 import EnsureAdmin from "../../../../Middlewares/EnsureAdmin";
 
 export = EmailRouter;
@@ -18,16 +19,32 @@ class EmailRouter
         this.router.post(`/send`, EnsureAdmin(), async (req, res) =>
         {
             // Filters
-            const { to, toActive, subject, body } = req.body;
+            const { to, cStatus, subject, body } = req.body;
+
+            // Validate
+            if (!to)
+                return APIError("Missing to")(res);
+
+            if (!to && !cStatus)
+                return APIError("Missing cStatus")(res);
+
+            if (!subject)
+                return APIError("Missing subject")(res);
+
+            if (!body)
+                return APIError("Missing body")(res);
+
 
             // Check if `to` is a email or `all`
             // If it is too all, then we check for all active customers
             if (to === "all")
             {
+                let filter = {};
+                if (cStatus && cStatus !== "all")
+                    filter = { status: cStatus };
+
                 // Get all customers
-                const customers = await CustomerModel.find({
-                    status: toActive ? "active" : "inactive",
-                });
+                const customers = await CustomerModel.find(filter);
 
                 // Send email to all customers
                 customers.forEach(async (customer) =>
@@ -36,10 +53,11 @@ class EmailRouter
                         receiver: customer.personal.email,
                         subject,
                         body: {
-                            body,
+                            body: await UseStyles(body),
                         },
                     });
                 });
+
                 return APISuccess(`Email sent to ${customers.length} customers`)(res);
             }
 
@@ -47,7 +65,7 @@ class EmailRouter
                 receiver: to,
                 subject,
                 body: {
-                    body: body,
+                    body: await UseStyles(body),
                 }
             });
 
