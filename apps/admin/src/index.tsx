35823d90c880d04ff4b410679d3e10a1e5b973aa
Tolfx <matteus@tolfx.dev>
2022-05-27 17:56:10 +0200

    feat: :sparkles: Adding `MUI` theme and css (#45)

--- apps/admin/src/index.tsx
+++ apps/admin/src/index.tsx
@@ -1,13 +1,20 @@
+import { createTheme, CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/system';
 import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 dotenv.config();
+const muiTheme = createTheme({
 
+});
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={muiTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
