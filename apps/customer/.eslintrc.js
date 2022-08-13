507e6c2312c7531d9ba405a3deed2811f23f6a73
Tolfx <matteus@tolfx.dev>
2022-08-13 13:53:54 +0200

    feat: small changes

--- apps/customer/.eslintrc.js
+++ apps/customer/.eslintrc.js
@@ -1,4 +1,12 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   root: true,
   extends: ["custom"],
+  "plugins": ["@typescript-eslint"],
+  rules: {
+    'import/no-anonymous-default-export': 0,
+    'react/display-name': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@next/next/no-img-element': 0,
+  }
 };
