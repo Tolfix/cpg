e3a479bb6fff7cd2bfed6fec34aec8ce790b5d57
Kai <21qtkai@gmail.com>
2022-10-11 20:54:26 +0100

    Added Docstring to SendEmail.

--- apps/api/src/Email/Send.ts
+++ apps/api/src/Email/Send.ts
@@ -4,9 +4,13 @@ import { IConfigs } from "interfaces/Admin/Configs.interface";
 import { AW } from "lib";
 import { Logger } from "lib";
 
+
 /**
- * @description
- * Send a email
+ * It sends an email to a receiver with a subject and body
+ * @param {string} receiver - The email address of the receiver.
+ * @param {string} subject - The subject of the email
+ * @param body - {
+ * @param [callback] - (error: Error | null, sent: boolean | null) => void
  */
 export async function SendEmail(
     receiver: string,
